c47d79585f50a2b176e2d784dcc300e97a1dc13d
softvert <cosmedam26@gmail.com>
2022-04-11 20:07:27 -0400

    comit tarea 1,2,3

---
+++ ob-ejercicios3-jpa/src/main/java/com/example/obejercicios3jpa/ObEjercicios3JpaApplication.java
@@ -0,0 +1,15 @@
+package com.example.obejercicios3jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObEjercicios3JpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObEjercicios3JpaApplication.class, args);
+	}
+
+	
+
+}
---
+++ ob-ejercicios3-jpa/src/test/java/com/example/obejercicios3jpa/ObEjercicios3JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.obejercicios3jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObEjercicios3JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
